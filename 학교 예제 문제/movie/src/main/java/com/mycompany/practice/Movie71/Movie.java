44a79743108288b13977b500e67913418f0f5d1f
Soeun0 <144868086+Soeun0@users.noreply.github.com>
2023-10-10 23:55:10 +0900

    movie 예제

---
+++ 학교 예제 문제/movie/src/main/java/com/mycompany/practice/Movie71/Movie.java
@@ -0,0 +1,10 @@
+package com.mycompany.practice.Movie71;
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+    
+    void playIt() {
+        System.out.println("영화상영을 시작합니다.");
+    }
+}
---
+++ 학교 예제 문제/movie/src/main/java/com/mycompany/practice/Practice.java
@@ -0,0 +1,10 @@
+package com.mycompany.practice;
+public class Practice {
+
+    public static void main(String[] args) {
+        double x = 3.5;
+        double y = 2.7;
+        int result = (int) (x+y);
+        System.out.println(result);
+    }
+}
